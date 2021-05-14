de44edc3acd8db1d8b1e620be342605f4bc2aa65
Rahul Agarwal <rahul123agarwal99@gmail.com>
2021-05-14 10:56:31 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Movie from "./Movie";
+
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=80cce31ca8f9a620527ba9296a3501c7";
+
+const IMAGE_API = "https://image.tmdb.org/t/p/w1280";
+
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=80cce31ca8f9a620527ba9296a3501c7&query=";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+  }, []);
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      fetch(SEARCH_API + searchTerm)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setMovies(data.results);
+        });
+
+      setSearchTerm("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit} action="">
+          <input
+            type="search"
+            className="search"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
       </header>
-    </div>
+      <div className="movie__container">
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
+    </>
   );
 }
 
 export default App;
+
+// Api stuff
+// featured : "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=80cce31ca8f9a620527ba9296a3501c7"
+// images : "https://image.tmdb.org/t/p/w1280"
+// search : "https://api.themoviedb.org/3/search/movie?&api_key=80cce31ca8f9a620527ba9296a3501c7&query="
